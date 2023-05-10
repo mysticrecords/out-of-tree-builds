147af7cfd0ae41d6d7c41206dd2ea98b1918bf87
Matt Walsh <mysticrecords@hotmail.com>
2023-05-10 18:54:42 +0100

    creating unit test using the boost unit test framework and incorporating this into cmake
    continuing the out of tree build setup

---
+++ projects/Array_Template_Classes/unittest/array_template_classesUT.cpp
@@ -0,0 +1,39 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE arrayTemplateClassTest
+
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+
+#include "../array_template_classes.h"
+
+BOOST_AUTO_TEST_CASE(test_fill) {
+    int testIntArr[5] {0};
+
+    Array<int, 5> intArr;
+    intArr.fill(0);
+
+    std::cout << intArr[0] << " == " << testIntArr[0] <<std::endl;
+    BOOST_CHECK(intArr[0] == testIntArr[0]);
+}
+
+BOOST_AUTO_TEST_CASE(test_set_fill) {
+    int testIntArr[5] {0};
+    testIntArr[3] = 10;
+
+    Array<int, 5> intArr {0};
+    intArr.set_fill(10, 3);
+
+    std::cout << intArr[3] << " == " << testIntArr[3] << std::endl;
+    BOOST_CHECK(intArr[3] == testIntArr[3]);
+}
+
+BOOST_AUTO_TEST_CASE(test_get_size) {
+    int testIntArr[5] {0};
+    int sizeOfTestIntArr = std::end(testIntArr) - std::begin(testIntArr);
+   
+    Array<int, 5> intArr {0};
+
+    std::cout << intArr.get_size() << " == " << sizeOfTestIntArr << std::endl;
+    BOOST_CHECK_EQUAL(intArr.get_size(), sizeOfTestIntArr);
+}
