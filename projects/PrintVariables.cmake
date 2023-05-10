7466821b63446536b5a035fc7825c4a5cbddf4f9
Matt Walsh <mysticrecords@hotmail.com>
2023-05-10 18:54:16 +0100

    creating unit test using the boost unit test framework and incorporating this into cmake
    continuing the out of tree build setup

--- projects/PrintVariables.cmake
+++ projects/PrintVariables.cmake
@@ -1,4 +1,5 @@
 function(print_variables)
+    message(STATUS "The CMake SOURCE directory is ${CMAKE_CURRENT_LIST_FILE}")
     message(STATUS "The CMake SOURCE directory is ${CMAKE_SOURCE_DIR}")
     message(STATUS "The CMake BINARY directory is ${CMAKE_BINARY_DIR}")
 endfunction()
