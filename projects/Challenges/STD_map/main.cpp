ad51a82056a4454654a007a6f84b3562d74e55d9
Matt Walsh <mysticrecords@hotmail.com>
2023-05-13 16:38:21 +0100

    test push

---
+++ projects/Challenges/STD_map/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+
+#include "Person.h"
+
+template<typename T>
+void display(std::map<T> &l) {
+    std::cout << "[ ";
+    for(auto el: l) {
+        std::cout << el << " ";
+    }
+    std::cout << " ]" << std::endl;
+};
+
+int main() {
+
+    std::set<Person> people{{"Curly",40}, {"Larry", 25}, {"Moe", 20}};
+
+    std::multiset<int> numbers {1,1,1,1,1,3,4,5,1,3,2,4,5,6,3};
+
+    
+
+    return 0;
+}
