c8e99873638b33d5b9c1094fc7042ec807d71b76
Matt Walsh <mysticrecords@hotmail.com>
2024-01-18 15:58:06 +0000

    Update utils.sh

--- .github/workflows/UTILS/utils.sh
+++ .github/workflows/UTILS/utils.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-AUTH='"${{ secrets.JENKINS_USER }}:${{ secrets.JENKINS_TOKEN }}"'
+AUTH='${{ secrets.JENKINS_USER }}:${{ secrets.JENKINS_TOKEN }}'
 CURL="curl -v -u \"$AUTH\""
-CURL_POST="\"$CURL\" -X POST"
+CURL_POST="$CURL -X POST"
 
 echo "URL=https://a040-92-13-233-78.ngrok-free.app" >> $GITHUB_ENV
 echo "AUTH=$AUTH" >> $GITHUB_ENV
