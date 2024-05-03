ab69481f882c0aa25df59e88a496a756fae72547
Matt Walsh <mysticrecords@hotmail.com>
2024-05-03 12:10:25 +0100

    Update main.cpp

--- projects/Section_13/src/main.cpp
+++ projects/Section_13/src/main.cpp
@@ -13,6 +13,9 @@ int main() {
     movies.add_movie("Jaws", "R", 1);
     movies.increment_watched("Jaws");
 
+     movies.add_movie("28 Days Later", "R", 1);
+    movies.increment_watched("28 Days Later");
+
     movies.display();
 
     return 0;
